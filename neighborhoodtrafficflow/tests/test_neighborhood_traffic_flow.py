168ec436e988c97abafbd87185232aedb2897574
Kareha Agesa <agesak@uw.edu>
2019-12-01 19:16:44 -0800

    fixing pytest.ini

--- neighborhoodtrafficflow/tests/test_neighborhood_traffic_flow.py
+++ neighborhoodtrafficflow/tests/test_neighborhood_traffic_flow.py
@@ -4,7 +4,7 @@ import importlib
 
 FIGURES = importlib.import_module("neighborhoodtrafficflow.figures")
 
-# def test_na_matplotlib_to_plotly():
-#     """Ensure color map has no nas"""
-#     colormap = FIGURES.matplotlib_to_plotly('viridis', 255)
-#     assert np.NaN not in colormap, "There are nas in the colormap"
+def test_na_matplotlib_to_plotly():
+    """Ensure color map has no nas"""
+    colormap = FIGURES.matplotlib_to_plotly('viridis', 255)
+    assert np.NaN not in colormap, "There are nas in the colormap"
