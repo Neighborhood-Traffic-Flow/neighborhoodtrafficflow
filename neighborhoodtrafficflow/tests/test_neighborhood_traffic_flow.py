e1c797feb007047959d1b2bdc104b8b628831e07
Kareha Agesa <agesak@uw.edu>
2019-12-01 19:21:52 -0800

    can i use a normal import now

--- neighborhoodtrafficflow/tests/test_neighborhood_traffic_flow.py
+++ neighborhoodtrafficflow/tests/test_neighborhood_traffic_flow.py
@@ -1,10 +1,9 @@
 """Tests"""
 import numpy as np
 import importlib
-
-FIGURES = importlib.import_module("neighborhoodtrafficflow.figures")
+from neighborhoodtrafficflow import matplotlib_to_plotly
 
 def test_na_matplotlib_to_plotly():
     """Ensure color map has no nas"""
-    colormap = FIGURES.matplotlib_to_plotly('viridis', 255)
+    colormap = matplotlib_to_plotly('viridis', 255)
     assert np.NaN not in colormap, "There are nas in the colormap"
