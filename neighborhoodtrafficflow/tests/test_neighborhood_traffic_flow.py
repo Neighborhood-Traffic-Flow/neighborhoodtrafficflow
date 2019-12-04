b31f029076a913c7367275c20f8db1d46b284040
Kelsey Maass <kmaass@uw.edu>
2019-12-04 15:55:11 -0800

    tryign somethign else

--- neighborhoodtrafficflow/tests/test_neighborhood_traffic_flow.py
+++ neighborhoodtrafficflow/tests/test_neighborhood_traffic_flow.py
@@ -1,6 +1,6 @@
 """Tests"""
 import numpy as np
-from .figures import matplotlib_to_plotly
+from ..figures import matplotlib_to_plotly
 
 def test_na_matplotlib_to_plotly():
     # commented for now to figure out what's wrong
