4643f065cea5aa2a4b73f94554b5bb8ba2387de5
Kelsey Maass <kmaass@uw.edu>
2019-12-07 15:21:30 -0800

    initializing tests for app

---
+++ neighborhoodtrafficflow/tests/test_app.py
@@ -0,0 +1,57 @@
+"""Test module for dashboard manager."""
+from pathlib import Path
+import pickle
+
+import numpy as np
+import pytest
+
+from neighborhoodtrafficflow.app import \
+    update_road_map_title, update_flow_count_title, \
+    update_speed_limit_title, update_road_type_title, \
+    update_dropdown, update_controls, update_neighborhood_map, \
+    update_road_map, update_traffic_flow_counts, update_speed_limits, \
+    update_road_types
+
+def test_update_road_map_title_1():
+    """Test something."""
+    pass
+
+def test_update_flow_count_title_1():
+    """Test something."""
+    pass
+
+def test_update_speed_limit_title_1():
+    """Test something."""
+    pass
+
+def test_update_road_type_title_1():
+    """Test something."""
+    pass
+
+def test_update_dropdown_1():
+    """Test something."""
+    pass
+
+def test_update_controls_1():
+    """Test something."""
+    pass
+
+def test_update_neighborhood_map_1():
+    """Test something."""
+    pass
+
+def test_update_road_map_1():
+    """Test something."""
+    pass
+
+def test_update_traffic_flow_counts_1():
+    """Test something."""
+    pass
+
+def test_update_speed_limits_1():
+    """Test something."""
+    pass
+
+def test_update_road_types_1():
+    """Test something."""
+    pass
