7f65e8aaebb93e75629b0f1f1015bb54623163df
Kelsey Maass <kmaass@uw.edu>
2019-12-07 15:19:36 -0800

    initializing chart tests

---
+++ neighborhoodtrafficflow/tests/test_charts.py
@@ -0,0 +1,21 @@
+"""Test module for generating charts."""
+from pathlib import Path
+import pickle
+
+import numpy as np
+import pytest
+
+from neighborhoodtrafficflow.figures.charts import \
+    traffic_flow_counts, speed_limits, road_types
+
+def test_traffic_flow_counts_1():
+    """Check blah."""
+    pass
+
+def test_speed_limits_1():
+    """Check something."""
+    pass
+
+def test_road_types_1():
+    """Check another thing."""
+    pass
